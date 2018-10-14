ad4b19218fc09a18049ea8bf1755e5ffb0ab549b
Lo Kemper <kemper.lb@gmail.com>
2018-10-14 09:16:24 +0200

    6th Oct

--- src/aux_functions/helpers.py
+++ src/aux_functions/helpers.py
@@ -8,13 +8,7 @@ import cv2
 
 SMALL_HEIGHT = 800
 
-def implt(img, cmp=None, t=''):
-    """ Show image using plt """
-    plt.imshow(img, cmap=cmp)
-    plt.title(t)
-    plt.show()
-
-
+   
 def resize(img, height=SMALL_HEIGHT, allways=False):
     """ Resize image to given height """
     if (img.shape[0] > height or allways):
@@ -41,3 +35,20 @@ def extendImg(img, shape):
     x = np.zeros(shape, np.uint8)
     x[:img.shape[0], :img.shape[1]] = img
     return x
+
+def display_image(img, cmap=None):
+    dpi = 80
+    margin = 0.05 # (5% of the width/height of the figure...)
+    xpixels, ypixels = 800, 800
+
+    # Make a figure big enough to accomodate an axis of xpixels by ypixels
+    # as well as the ticklabels, etc...
+    figsize = (1 + margin) * ypixels / dpi, (1 + margin) * xpixels / dpi
+
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    # Make the axis the right size...
+    ax = fig.add_axes([margin, margin, 1 - 2*margin, 1 - 2*margin])
+
+    ax.imshow(img, cmap=cmap)
+    plt.show()
+    return print("loaded")
